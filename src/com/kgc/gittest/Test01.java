c06bae9e6fe28f6b7d2da5da6b7dad8fe2a8f622
王佳浩 <1397673599@qq.com>
2021-07-23 17:39:57 +0800

    对test01进行了修改

--- src/com/kgc/gittest/Test01.java
+++ src/com/kgc/gittest/Test01.java
@@ -4,4 +4,9 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
+    public void sum() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
 }
